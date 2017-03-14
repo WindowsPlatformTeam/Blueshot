9b27f41ce20d0ab3fa40764b80fca433c29ee891
maktub82 <gallardo91_@hotmail.com>
2017-03-14 21:43:17 +0100

    Add Player

---
+++ src/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;
+
+    private Rigidbody2D _rigidBody2D;
+
+    private void Start()
+    {
+        _rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (_rigidBody2D == null) return;
+
+        var horizontalMovement = Input.GetAxis("Horizontal");
+        var verticalMovement = Input.GetAxis("Vertical");
+
+        _rigidBody2D.velocity = new Vector2(horizontalMovement * Speed, verticalMovement * Speed);
+    }
+}
