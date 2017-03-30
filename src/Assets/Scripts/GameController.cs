16951ab6cc9ca545d0e65e455c33eda12bb6c999
Sergio Gallardo Sales <gallardo91_@hotmail.com>
2017-03-30 13:30:15 +0200

    Add GameOver and Restart (#5)

--- src/Assets/Scripts/GameController.cs
+++ src/Assets/Scripts/GameController.cs
@@ -1,11 +1,14 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
     public Text ScoreText;
+    public GameObject gameOverPanel;
 
     private int _score;
+    private bool _isGameOver;
 
     public static GameController GetInstance()
     {
@@ -17,9 +20,30 @@ public class GameController : MonoBehaviour
         return null;
     }
 
+    public void Update()
+    {
+        if (_isGameOver && Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+    }
+
     public void AddScore(int score)
     {
         _score += score;
         ScoreText.text = string.Format("Score: {0}", _score);
+    }
+
+    public void GameOver()
+    {
+        _isGameOver = true;
+        gameOverPanel.SetActive(true);
+    }
+
+    public void Restart()
+    {
+        _isGameOver = false;
+        gameOverPanel.SetActive(false);
+        SceneManager.LoadScene("MainScene");
     }
 }
