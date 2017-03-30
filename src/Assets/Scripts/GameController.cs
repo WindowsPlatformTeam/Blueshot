de2ca7193b4088219dcfec38af709eb0ff29cc0c
Sergio Gallardo Sales <sgallardo@plainconcepts.com>
2017-03-30 12:18:00 +0200

    Add Score

---
+++ src/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public Text ScoreText;
+
+    private int _score;
+
+    public static GameController GetInstance()
+    {
+        var gameControllerGameObject = GameObject.FindWithTag("GameController");
+        if (gameControllerGameObject != null)
+        {
+            return gameControllerGameObject.GetComponent<GameController>();
+        }
+        return null;
+    }
+
+    public void AddScore(int score)
+    {
+        _score += score;
+        ScoreText.text = string.Format("Score: {0}", _score);
+    }
+}
