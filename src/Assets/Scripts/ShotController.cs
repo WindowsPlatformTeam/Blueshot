444cb42a7ec8e93bd2763bfa720a9250cc685961
Sergio Gallardo Sales <gallardo91_@hotmail.com>
2017-03-30 11:27:51 +0200

    Feature/shot (#2)
    * Add Shot
    
    * Add Rotation in Player (WIP)

--- src/Assets/Scripts/ShotController.cs
+++ src/Assets/Scripts/ShotController.cs
@@ -3,9 +3,12 @@
 public class ShotController : MonoBehaviour
 {
     public Material Material;
+    public Material ShotMaterial;
     public float MaxDistance = 10;
+    public float ShotCooldown = 0.5f;
 
-    private LineRenderer line;
+    private LineRenderer _line;
+    private float _nextShot;
 
     private void Start()
     {
@@ -14,35 +17,62 @@ public class ShotController : MonoBehaviour
 
     void Update()
     {
-        DrawShotIndicator();
+        var ray = RayShotIndicator();
+        ShotControl(ray);
     }
 
     private void CreateLine()
     {
-        line = new GameObject("ShotIndicator").AddComponent<LineRenderer>();
-        line.material = Material;
-        line.numPositions = 2;
-        line.startWidth = 0.04f;
-        line.endWidth = 0.04f;
-        line.useWorldSpace = true;
+        _line = new GameObject("ShotIndicator").AddComponent<LineRenderer>();
+        _line.material = Material;
+        _line.numPositions = 2;
+        _line.startWidth = 0.04f;
+        _line.endWidth = 0.04f;
+        _line.useWorldSpace = true;
     }
 
-    private void DrawShotIndicator()
+    private RaycastHit2D RayShotIndicator()
     {
         var mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var direction = mousePos - transform.position;
         var ray = new Ray2D(transform.position, direction);
         var hit = Physics2D.Raycast(transform.position, direction, MaxDistance);
 
-        line.SetPosition(0, ray.origin);
+        _line.SetPosition(0, ray.origin);
 
         if (hit.collider != null)
         {
-            line.SetPosition(1, hit.point);
+            _line.SetPosition(1, hit.point);
         }
         else
         {
-            line.SetPosition(1, ray.GetPoint(MaxDistance));
+            _line.SetPosition(1, ray.GetPoint(MaxDistance));
         }
+
+        return hit;
+    }
+
+    private void ShotControl(RaycastHit2D hit)
+    {
+        if (Time.time < _nextShot)
+        {
+            _line.material = ShotMaterial;
+            return;
+        }
+
+        if (Input.GetButton("Fire1"))
+        {
+            _nextShot = Time.time + ShotCooldown;
+            Shot(hit.collider);
+        }
+
+        _line.material = Material;
+    }
+
+    private void Shot(Collider2D collider)
+    {
+        if (collider == null) return;
+
+        Destroy(collider.gameObject);
     }
 }
